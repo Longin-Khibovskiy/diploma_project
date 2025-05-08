c7689ad4445a6dadac73f70b5285d6e1d9c86ce6
Longin <l.khibovskiy@gmail.com>
2025-05-08 20:33:21 +0300

    add .env

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+### Команда для создания и подключения к БД ###
+#### Если ваши данные от БД отличаются, то замените их своими ####
+```
+# 1 раз при первом запуске
+echo "DB_HOST=localhost" >> .env
+echo "DB_USER=root" >> .env
+echo "DB_PASS=root" >> .env
+echo "DB_NAME=Collection" >> .env
+```
+
 ### Команда для заполнения базы данных ###
 ```
 php config/seeds.php
--- config/connect.php
+++ config/connect.php
@@ -1,19 +1,23 @@
 <?php
 global $link;
 
-if (!file_exists(__DIR__ . '/.env')) {
-    die('.env file not found. Create it and define DB_HOST, DB_USER, DB_PASS, DB_NAME.');
+$envPath = dirname(__DIR__) . '/.env';
+if (!file_exists($envPath)) {
+    die(".env  файл не найден по пути $envPath");
 }
-$env = parse_ini_file(__DIR__ . '/.env');
+
+$env = parse_ini_file($envPath);
+
 $requiredVars = ['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME'];
 foreach ($requiredVars as $var) {
     if (!isset($env[$var])) {
-        die("Environment variable $var is not set.");
+        die("Переменная $var не задана в файле .env.");
     }
 }
+
 $link = mysqli_connect($env['DB_HOST'], $env['DB_USER'], $env['DB_PASS'], $env['DB_NAME']);
 
-if ($link == false) {
-    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+if ($link === false) {
+    die('Ошибка: Невозможно подключиться к MySQL ' . mysqli_connect_error());
 }
 ?>
