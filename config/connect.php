e2579122a76f2c21cf7eaeeac256c2d1f9adad2d
Longin <l.khibovskiy@gmail.com>
2025-05-08 20:17:38 +0300

    .gitignore is now working

--- config/connect.php
+++ config/connect.php
@@ -1,8 +1,19 @@
 <?php
 global $link;
-$link = mysqli_connect("localhost", "admin", "admin", "Collection");
 
-if ($link == false){
+if (!file_exists(__DIR__ . '/.env')) {
+    die('.env file not found. Create it and define DB_HOST, DB_USER, DB_PASS, DB_NAME.');
+}
+$env = parse_ini_file(__DIR__ . '/.env');
+$requiredVars = ['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME'];
+foreach ($requiredVars as $var) {
+    if (!isset($env[$var])) {
+        die("Environment variable $var is not set.");
+    }
+}
+$link = mysqli_connect($env['DB_HOST'], $env['DB_USER'], $env['DB_PASS'], $env['DB_NAME']);
+
+if ($link == false) {
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
 }
 ?>
