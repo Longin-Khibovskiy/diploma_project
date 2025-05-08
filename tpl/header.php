1974e6aa3627e6dedbb0b5707612d289d3fe967a
Longin <l.khibovskiy@gmail.com>
2025-05-08 20:47:04 +0300

    add logo

--- tpl/header.php
+++ tpl/header.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Collaboration</title>
+    <link rel="icon" href="/images/logo.png">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css'>
     <link rel="stylesheet" href="/stylesheet/index.css">
     <?php
